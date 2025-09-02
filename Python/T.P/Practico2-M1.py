1eda94519591ca3266f1a6d6b1bed284e9709511
JosephV95 <viltejl@gmail.com>
2025-09-01 22:31:22 -0300

    Terminado ejercicio 1 del TP2

---
+++ Python/T.P/Practico2-M1.py
@@ -0,0 +1,54 @@
+"""Estructuras de Datos (listas, tuplas, diccionarios)
+1. Solicitar al usuario que ingrese números, los cuales se guardarán en una lista. Finalizar al ingresar el número 0, el cual no debe guardarse. 
+Luego de que se termina la carga de la lista, solicitar al usuario otro número y crear una lista con los elementos de la lista original que 
+sean menores que el número dado. Imprimir esta nueva lista, iterando por ella."""
+
+# listaNumeros = []
+# listaFinal = []
+
+# numero = int(input("Ingrese un numero: "))
+
+# while numero != 0:
+#     listaNumeros.append(numero)
+#     numero = int(input("Ingrese un otro numero (para terminar ingrese 0): "))
+    
+# numero2 = int(input("Ingrese otro numero para comparar: "))
+
+# for num in listaNumeros:
+#     if num < numero2:
+#         listaFinal.append(num)
+
+# for i in listaFinal:
+#     print(i)
+
+"""2. Leer una secuencia de 10 números, almacenarlos en una lista y mostrar la suma de los elementos que ocupan posiciones pares y el 
+mayor número de los queocupan posiciones impares. """
+lista num = []
+
+
+"""3. Dadas 2 listas A y B de igual número de elementos, se desea generar e imprimir una lista C conteniendo 
+las sumas: A[i] + B[i] = C[i]. También indicar (solo imprimir por pantalla) aquellos elementos que están en A y no están en B."""
+
+"""4. Dado una lista de 10 nombres de personas, realice un programa que cargue la
+lista, la ordene de forma ascendente y la muestre por pantalla ordenado. Python
+nos brinda la función “sorted” para realizar dicho procedimiento, pero la idea es
+que el ejercicio se resuelva utilizando algoritmia propia de algún método de
+ordenamiento existente.
+5. Escribir un programa que permita cargar y procesar datos de alumnos del ITU en
+una lista de tuplas con la siguiente forma: (nombre, dni, materia). Ejemplo:
+[(“Manuel Juarez”, 19823451, “Matematica”), (“Silvana Paredes”, 22709128,
+“Programacion”), (“Rosa Ortiz”, 15123978, “Redes”), (“Luciana Hernandez”,
+38981374, “Programacion”)]. Hacer un menú iterativo que permita al usuario
+realizar las siguientes operaciones:
+ Agregar alumnos a la lista.
+ Dado el DNI de un alumno, ver las materias que cursa.
+ Dada una materia, mostrar la cantidad de alumnos que la cursan.
+6. Cree un diccionario con los nombres de 5 personas de su familia y sus edades.
+Indicar el integrante más grande y el mas chico.
+7. Cree un diccionario que contenga el nombre de una ciudad, el país al que
+pertenece y la cantidad de habitantes que tiene. Hacer un menú iterativo que
+permita al usuario realizar las siguientes operaciones:
+ Agregar ciudades
+ Eliminar ciudades
+ Indicar la cantidad de habitantes en un país en particular
+ El porcentaje de habitantes en una ciudad de acuerdo al total registrado """
