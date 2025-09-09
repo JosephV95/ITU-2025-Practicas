6f7cfd6f7f67bd304b79f38fdad53e00796bdaac
JosephV95 <viltejl@gmail.com>
2025-09-09 11:20:33 -0300

    Ejercicio 4 terminado

--- Python/T.P/Practico2-M1.py
+++ Python/T.P/Practico2-M1.py
@@ -46,32 +46,32 @@ mayor número de los queocupan posiciones impares. """
 las sumas: A[i] + B[i] = C[i]. También indicar (solo imprimir por pantalla) aquellos elementos que están en A y no están en B."""
 
 #! A la entrada no se la convirtio en int para que pueda guardar un string
-entrada = input("Ingrese un numero para la lista A (presione Z para cancelar): ") 
-lista_A = []; lista_B = []; lista_C = []
+# entrada = input("Ingrese un numero para la lista A (presione Z para cancelar): ") 
+# lista_A = []; lista_B = []; lista_C = []
 
-while entrada.lower() != "z": #! Se convierte la entrada en minuscula y luego si no es un string se la convierte a numero con int()
-    lista_A.append( int(entrada) )
-    entrada = input("Ingrese otro numero para la lista A (presione Z para terminar): ")
+# while entrada.lower() != "z": #! Se convierte la entrada en minuscula y luego si no es un string se la convierte a numero con int()
+#     lista_A.append( int(entrada) )
+#     entrada = input("Ingrese otro numero para la lista A (presione Z para terminar): ")
 
-for num in range( len(lista_A) ):
-    numeroB = int(input("Ingrese un numero para la lista B: "))
-    lista_B.append(numeroB)
+# for num in range( len(lista_A) ):
+#     numeroB = int(input("Ingrese un numero para la lista B: "))
+#     lista_B.append(numeroB)
     
-for indice_A, indice_B in zip(lista_A, lista_B):
-    lista_C.append( indice_A + indice_B)
+# for indice_A, indice_B in zip(lista_A, lista_B):
+#     lista_C.append( indice_A + indice_B)
     
-print("La lista A es:",lista_A)
-print("La lista B es:",lista_B)
-print("La lista C es:",lista_C)
+# print("La lista A es:",lista_A)
+# print("La lista B es:",lista_B)
+# print("La lista C es:",lista_C)
 
-for i in lista_A:
-    esta = 0
-    for j in lista_B:
-        if i == j:
-            esta = 1
-            break
-    if esta == 0:
-        print("El elemento:", i , "no esta en la lista B")
+# for i in lista_A:
+#     esta = 0
+#     for j in lista_B:
+#         if i == j:
+#             esta = 1
+#             break
+#     if esta == 0:
+#         print("El elemento:", i , "no esta en la lista B")
 
 """4. Dado una lista de 10 nombres de personas, realice un programa que cargue la
 lista, la ordene de forma ascendente y la muestre por pantalla ordenado. Python
@@ -79,17 +79,31 @@ nos brinda la función “sorted” para realizar dicho procedimiento, pero la i
 que el ejercicio se resuelva utilizando algoritmia propia de algún método de
 ordenamiento existente."""
 
-# lista_nombres = []
+lista_nombres = ["maria", "jose", "susana", "Eli", "lucy", "sara", "luna"]
+auxiliar = ""
 
 # for i in range(5):
 #     nombre = input(f"Ingese el nombre nro {i+1}: ")
 #     lista_nombres.append(nombre.lower())
 # print(sorted(lista_nombres))
 
-# if 'a' > 'z':
-#     print('a')
-# else:
-#     print('z')
+for i in range(len(lista_nombres)):
+    contador_de_iteracion = 0 #No es necesario ponerlo, solo quiero que cuente la cantidad de cambios por iteracion
+    
+    for j in range( i+1, len(lista_nombres)):
+        # print("Comparo", lista_nombres[i] ,"con", lista_nombres[j])
+        
+        if lista_nombres[i].upper() > lista_nombres[j].upper(): #! El sistema lee las palabras como numeros, es decir se pueden comparar
+            auxiliar = lista_nombres[i]
+            lista_nombres[i] = lista_nombres[j].upper()
+            lista_nombres[j] = auxiliar.upper()
+            print(lista_nombres)
+            contador_de_iteracion += 1
+    
+    print(f"En la iteracion nro {i+1} se hicieron {contador_de_iteracion} cambios")
+print ("La lista final ordenada queda asi:" , lista_nombres)
+        
+    
 
 """5. Escribir un programa que permita cargar y procesar datos de alumnos del ITU en
 una lista de tuplas con la siguiente forma: (nombre, dni, materia). Ejemplo:
