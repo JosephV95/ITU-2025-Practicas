fac81a1b15f1404095c708e6b5e05416d6088f17
JosephV95 <viltejl@gmail.com>
2025-11-03 11:48:11 -0300

    completado funciones para ver promedio

--- Html_Css_JS/Ejercicios_Practica_teoria/Ejercicio_5.js
+++ Html_Css_JS/Ejercicios_Practica_teoria/Ejercicio_5.js
@@ -1,14 +1,41 @@
-function habilitarNotas(materia){
-    var fila = document.getElementById(materia);
-    var notas = fila.querySelectorAll(".nota");
-    var checkbox = fila.querySelector('input[type="checkbox"]');
-    var botonPromedio = fila.querySelector("input[type='button']");
+function habilitarNotas(materia) {
+  var fila = document.getElementById(materia);
+  var notas = fila.querySelectorAll(".nota");
+  var checkbox = fila.querySelector('input[type="checkbox"]');
+  var botonPromedio = fila.querySelector("input[type='button']");
+  var mensajePromedio = fila.querySelector(".mensajePromedio");
 
-    if ( checkbox.checked){
-        notas.forEach(nota => {
-            nota.disabled = false
-        });
-    } else {
-        notas.forEach( nota => nota.disabled = true)
-    }
+  if (checkbox.checked) {
+    notas.forEach((nota) => {
+      nota.disabled = false;
+    });
+
+    botonPromedio.disabled = false;
+    mensajePromedio.disabled =false
+  } else {
+    notas.forEach((nota) => (nota.disabled = true));
+
+    botonPromedio.disabled = true;
+    mensajePromedio.disabled = true;
+  }
+}
+
+function verPromedio(materia) {
+  var fila = document.getElementById(materia);
+  var listaNotas = fila.querySelectorAll(".nota");
+
+  var sumaNotas = 0;
+  var totalNotas = 0;
+  console.log(listaNotas);
+  
+
+  listaNotas.forEach((nota) => {
+    sumaNotas += parseInt(nota.value);
+    totalNotas++;
+  });
+
+  promedio = ( sumaNotas / totalNotas ).toFixed(2);
+
+  fila.querySelector(".mensajePromedio").value = promedio;
+
 }
