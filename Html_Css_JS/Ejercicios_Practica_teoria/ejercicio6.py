b3e1deb66f8238fd18d818657f84e528c5c8811b
JosephV95 <viltejl@gmail.com>
2025-11-08 15:57:30 -0300

    Ejercicio 6 terminado

--- Html_Css_JS/Ejercicios_Practica_teoria/ejercicio6.py
+++ Html_Css_JS/Ejercicios_Practica_teoria/ejercicio6.py
@@ -12,17 +12,25 @@ def pag_operacion():
         numero_1 = int(request.args.get("numero_1"))
         numero_2 = int(request.args.get("numero_2"))
         operacion = request.args.get("operacion")
+    
+    if request.method == 'POST':
+        numero_1 = int(request.form["numero_1"])
+        numero_2 = int(request.form["numero_2"])
+        operacion = request.form["operacion"]
         
-        mensaje = f'<h1>Los numeros son:  {numero_1} y {numero_2} y la operacion es: {operacion}</h1> <br>'
+    mensaje = f'<h1>Los numeros son:  {numero_1} y {numero_2} y la operacion es: {operacion}</h1> <br>'
         
-        if operacion == "Suma":
-            return f'{mensaje} <h2>El resultado es:  {numero_1 + numero_2}'
-        elif operacion == "Resta":
-            return f'{mensaje} <h2>El resultado es:  {numero_1 - numero_2}'
-        elif operacion == "Multiplicacion":
-            return f'{mensaje} <h2>El resultado es:  {numero_1 * numero_2}'
-        else :
-            return f'{mensaje} <h2>El resultado es:  {numero_1 / numero_2}'
+    if operacion == "Suma":
+        return f'{mensaje} <h2>El resultado es:  {numero_1 + numero_2}'
+    elif operacion == "Resta":
+        return f'{mensaje} <h2>El resultado es:  {numero_1 - numero_2}'
+    elif operacion == "Multiplicacion":
+        return f'{mensaje} <h2>El resultado es:  {numero_1 * numero_2}'
+    else :
+        return f'{mensaje} <h2>El resultado es:  {numero_1 / numero_2}'
+        
+    
+        
 
 if __name__ == "__main__":
     app.run(debug=True)
